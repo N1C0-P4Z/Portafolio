49efdcf4b447e85b62a72c6910c2c4fa97e803e6
N1C0-P4Z <rubenpaz2105@gmail.com>
2024-08-20 19:52:19 -0300

    Add files via upload

---
+++ HelloWord.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Ingrese un entero: ";
+    cin >> N;    
+    cout << "Se ingreso un " << N;
+    return 0; 
+} 
